7d12f5a783543487dbcb329fa9305ba986386428
Cody Tavenner <32970224+ctavenne14@users.noreply.github.com>
2020-08-10 20:25:45 -0700

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h3>
+              XML TryIt </h3>
+            <h4>
+                Takes URL of XML file and returns the XML elements and attributes.</h4>
+    <p>URL Input:<asp:TextBox ID="TextBox1" runat="server" Width="315px">http://www.public.asu.edu/~ctavenne/Books.xml</asp:TextBox>
+&nbsp;<asp:Button ID="ButtonUpload" runat="server" Text="Upload" OnClick="ButtonUpload_Click" />
+        
+    </p>
+    <p>Upload Status:
+        <asp:Label ID="lblUpload" runat="server"></asp:Label>
+    </p>
+
+    <p>
+        <asp:TextBox ID="TextBox2" runat="server" Height="121px" Rows="20" TextMode="MultiLine" Width="721px"></asp:TextBox>
+    </p>
+    <p>Book information gathered from https://www.barnesandnoble.com/b/books/_/N-1fZ29Z8q8
+    </p>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+public partial class _Default : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void ButtonUpload_Click(object sender, EventArgs e)
+    {
+        if(!String.IsNullOrWhiteSpace(TextBox1.Text))
+        {
+            string url = TextBox1.Text;
+            XmlDocument xmlDoc = new XmlDocument();
+            try
+            {
+                xmlDoc.Load(url);
+            }
+            catch (XmlException ex)
+            {
+                lblUpload.Text = ex.Message;
+            }
+            StringWriter sw = new StringWriter();
+            XmlTextWriter xw = new XmlTextWriter(sw);
+            xmlDoc.WriteTo(xw);
+            string doc; 
+            doc = sw.ToString();
+            doc.Replace(">", ">" + "\r\n");
+            TextBox2.Text = doc;
+        }
+        else
+        {
+            lblUpload.Text = "Text Box can not be empty";
+        }
+    }
+}
